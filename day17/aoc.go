63ffc9982add51effc107d385488289ff9bac37b
Axel Wagner <mero@merovius.de>
2020-12-18 10:30:11 +0100

    day 17: Fix bugs introduced in stream
    When trying to generalize over the dimension of the Graph in a single
    type, I introduced two bugs:
    
    • In the WalkNeighbors implementation, I intended to skip the zero
      offset when calling the func, but I also skipped incrementing the
      offset as well, leading to an endless loop
    • fmt.Sprint space-, not comma-separates slice-elements, so cellFromKey
      was wrong. This was hidden by ignoring the error from strconv.Atoi.
      I actually predicted that something like this would happen on stream :)
    
    This commit fixes these two bugs and adds a couple of tests I wrote for
    debugging.

--- day17/aoc.go
+++ day17/aoc.go
@@ -48,12 +48,10 @@ type Cell []int
 
 // Grid is a grid of a fixed dimension.
 type Grid struct {
-	// m is a set, with fmt.Sprintf("%v",c) as a key, where c is a []int.
+	// m is a set, with fmt.Sprint(c) as a key, where c is a Cell.
 	m map[string]bool
 	// dim is the dimension of the grid
 	dim int
-	min []int
-	max []int
 }
 
 // NewGrid returns a new Grid of the given dimension.
@@ -68,12 +66,19 @@ func (g *Grid) SetAlive(c Cell) {
 	g.m[fmt.Sprint(c)] = true
 }
 
+func (g *Grid) Alive(c Cell) bool {
+	return g.m[fmt.Sprint(c)]
+}
+
 func (g *Grid) cellFromKey(k string) Cell {
 	k = k[1 : len(k)-1]
-	sp := strings.Split(k, ",")
+	sp := strings.Split(k, " ")
 	c := make([]int, g.dim)
 	for i, s := range sp {
-		v, _ := strconv.Atoi(s)
+		v, err := strconv.Atoi(s)
+		if err != nil {
+			panic(fmt.Errorf("invalid key %q: %w", k, err))
+		}
 		c[i] = v
 	}
 	return c
@@ -88,7 +93,7 @@ func (g *Grid) Simulate() {
 			next[k] = true
 		}
 		g.WalkNeighbors(c, func(c Cell) {
-			if g.m[fmt.Sprint(c)] {
+			if g.Alive(c) {
 				return
 			}
 			if n := g.CountAliveNeighbors(c); n == 3 {
@@ -107,15 +112,17 @@ func (g *Grid) WalkNeighbors(c Cell, f func(Cell)) {
 
 idxLoop:
 	for {
+		var c2 Cell
 		if isZero(idx) {
-			continue
+			goto inc
 		}
-		c2 := make(Cell, g.dim)
+		c2 = make(Cell, g.dim)
 		copy(c2, c)
 		for i := range c {
 			c2[i] += idx[i]
 		}
 		f(c2)
+	inc:
 		for i := 0; i < len(idx); i++ {
 			idx[i]++
 			if idx[i] <= 1 {
@@ -130,7 +137,7 @@ idxLoop:
 func (g *Grid) CountAliveNeighbors(c Cell) int {
 	var n int
 	g.WalkNeighbors(c, func(c Cell) {
-		if g.m[fmt.Sprint(c)] {
+		if g.Alive(c) {
 			n++
 		}
 	})
@@ -153,6 +160,7 @@ func (g *Grid) Bounds() (min, max Cell) {
 	}
 	for k := range g.m {
 		c := g.cellFromKey(k)
+		fmt.Println(c)
 		for i := range c {
 			if c[i] < min[i] {
 				min[i] = c[i]
@@ -167,4 +175,88 @@ func (g *Grid) Bounds() (min, max Cell) {
 
 func (g *Grid) AliveCells() int {
 	return len(g.m)
+}
+
+func (g *Grid) Dump() {
+	min, max := g.Bounds()
+	c := append(Cell(nil), min...)
+planeLoop:
+	for {
+		if g.dim > 2 {
+			// Assemble the plane-identifier ("x2=23, x3=42:")
+			var plane []string
+			for i := 2; i < len(c); i++ {
+				plane = append(plane, fmt.Sprintf("x%d=%d", i, c[i]))
+			}
+			fmt.Printf("%s:\n", strings.Join(plane, ", "))
+		}
+		for c[1] = min[1]; c[1] <= max[1]; c[1]++ {
+			for c[0] = min[0]; c[0] <= max[0]; c[0]++ {
+				if g.Alive(c) {
+					fmt.Print("#")
+				} else {
+					fmt.Print(".")
+				}
+			}
+			fmt.Println()
+		}
+
+		for i := 2; i < len(c); i++ {
+			c[i]++
+			if c[i] <= max[i] {
+				continue planeLoop
+			}
+			c[i] = min[i]
+		}
+		break
+	}
+}
+
+func (c Cell) cmp(c2 Cell) int {
+	if len(c) != len(c2) {
+		panic("comparing cells of different dimension")
+	}
+	for i := range c {
+		if c[i] < c2[i] {
+			return -1
+		} else if c[i] > c2[i] {
+			return 1
+		}
+	}
+	return 0
+}
+
+func (c Cell) less(c2 Cell) bool {
+	return c.cmp(c2) < 0
+}
+
+// dist returns the distance of c and c2 in the maximum norm.
+func (c Cell) dist(c2 Cell) int {
+	if len(c) != len(c2) {
+		panic("comparing cells of different dimension")
+	}
+	var d int
+	for i := range c {
+		δ := c[i] - c2[i]
+		if δ < 0 {
+			δ = -δ
+		}
+		if δ > d {
+			d = δ
+		}
+	}
+	return d
+}
+
+func (c Cell) eq(c2 Cell) bool {
+	return c.cmp(c2) == 0
+}
+
+func (c Cell) zero() bool {
+	for _, v := range c {
+		if v != 0 {
+			return false
+		}
+	}
+	return true
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Merovius/aoc_2020
 
 go 1.15
+
+require github.com/google/go-cmp v0.5.4
