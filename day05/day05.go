d9e4594bc873b8dff5b8de4604277eb57b3c5064
Axel Wagner <mero@merovius.de>
2022-12-05 10:36:13 +0100

    day05: parse stacks as well

--- day05/day05.go
+++ day05/day05.go
@@ -1,53 +1,24 @@
 package main
 
 import (
+	"bufio"
 	_ "embed"
-	"flag"
+	"errors"
 	"fmt"
 	"io"
 	"log"
-	"strings"
+	"os"
+
+	"gonih.org/stack"
 )
 
-//go:embed example.txt
-var exampleFile string
-
-//go:embed input.txt
-var inputFile string
-
-var exampleStacks = [][]byte{
-	{'Z', 'N'},
-	{'M', 'C', 'D'},
-	{'P'},
-}
-
-var inputStacks = [][]byte{
-	{'B', 'P', 'N', 'Q', 'H', 'D', 'R', 'T'},
-	{'W', 'G', 'B', 'J', 'T', 'V'},
-	{'N', 'R', 'H', 'D', 'S', 'V', 'M', 'Q'},
-	{'P', 'Z', 'N', 'M', 'C'},
-	{'D', 'Z', 'B'},
-	{'V', 'C', 'W', 'Z'},
-	{'G', 'Z', 'N', 'C', 'V', 'Q', 'L', 'S'},
-	{'L', 'G', 'J', 'M', 'D', 'N', 'V'},
-	{'T', 'P', 'M', 'F', 'Z', 'C', 'G'},
-}
-
 func main() {
-	input := flag.Bool("input", false, "run on input code")
-	flag.Parse()
-	var (
-		stacks [][]byte
-		insts  []Inst
-		err    error
-	)
-	if *input {
-		stacks = inputStacks
-		insts, err = ReadInstructions(strings.NewReader(inputFile))
-	} else {
-		stacks = exampleStacks
-		insts, err = ReadInstructions(strings.NewReader(exampleFile))
+	s := bufio.NewScanner(os.Stdin)
+	stacks, err := ReadStacks(s)
+	if err != nil {
+		log.Fatal(err)
 	}
+	insts, err := ReadInstructions(s)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -55,25 +26,69 @@ func main() {
 	fmt.Printf("Tops of the stacks: %q\n", Tops(stacks))
 }
 
-func ReadInstructions(r io.Reader) ([]Inst, error) {
+func ReadStacks(s *bufio.Scanner) ([]stack.Stack[byte], error) {
+	var lines [][]byte
+scanloop:
+	for s.Scan() {
+		s := s.Text()
+		if s == "" {
+			break
+		}
+		var line []byte
+		for i := 0; i < len(s); i += 4 {
+			cell := s[i : i+3]
+			if cell == "   " {
+				line = append(line, 0)
+			} else if cell == " 1 " {
+				continue scanloop
+			} else if cell[0] != '[' || cell[2] != ']' {
+				return nil, fmt.Errorf("invalid stack line %q", s)
+			} else {
+				line = append(line, cell[1])
+			}
+		}
+		lines = append(lines, line)
+	}
+	if len(lines) == 0 {
+		return nil, errors.New("no stacks given")
+	}
+	n := len(lines[0])
+	for i, l := range lines[1:] {
+		if len(l) != n {
+			return nil, fmt.Errorf("number of stacks is not consistent along lines (line 1 -> %d, line %d -> %d)", n, i+1, len(l))
+		}
+	}
+	stacks := make([]stack.Stack[byte], n)
+	for i := len(lines) - 1; i >= 0; i-- {
+		for j, b := range lines[i] {
+			if b == 0 {
+				continue
+			}
+			stacks[j].Push(b)
+		}
+	}
+	return stacks, nil
+}
+
+func ReadInstructions(s *bufio.Scanner) ([]Inst, error) {
 	var out []Inst
-	for n := 0; ; n++ {
+	for s.Scan() {
 		var i Inst
-		j, err := fmt.Fscanf(r, "move %d from %d to %d\n", &i.N, &i.From, &i.To)
+		j, err := fmt.Sscanf(s.Text(), "move %d from %d to %d\n", &i.N, &i.From, &i.To)
 		if err == io.EOF || err == io.ErrUnexpectedEOF {
 			break
 		}
 		if j != 3 || err != nil {
-			return nil, fmt.Errorf("line %d: %w", n+1, err)
+			return nil, err
 		}
 		i.From -= 1
 		i.To -= 1
 		out = append(out, i)
 	}
-	return out, nil
+	return out, s.Err()
 }
 
-type Stacks [][]byte
+type Stacks []stack.Stack[byte]
 
 type Inst struct {
 	N    int
@@ -81,33 +96,24 @@ type Inst struct {
 	To   int
 }
 
-func Exec(stacks [][]byte, prog []Inst) {
-	pop := func(i int) (v byte) {
-		n := len(stacks[i]) - 1
-		stacks[i], v = stacks[i][:n], stacks[i][n]
-		return v
-	}
-	push := func(i int, b byte) {
-		stacks[i] = append(stacks[i], b)
-	}
+func Exec(stacks []stack.Stack[byte], prog []Inst) {
+	var buf stack.Stack[byte]
 	for _, inst := range prog {
-		var buf []byte
 		for j := 0; j < inst.N; j++ {
-			buf = append(buf, pop(inst.From))
+			buf.Push(stacks[inst.From].Pop())
 		}
-		for j := len(buf) - 1; j >= 0; j-- {
-			push(inst.To, buf[j])
+		for len(buf) > 0 {
+			stacks[inst.To].Push(buf.Pop())
 		}
 	}
 }
 
-func Tops(stacks [][]byte) string {
+func Tops(stacks []stack.Stack[byte]) string {
 	var out []byte
 	for _, s := range stacks {
-		if len(s) == 0 {
-			continue
+		if len(s) > 0 {
+			out = append(out, s.Top())
 		}
-		out = append(out, s[len(s)-1])
 	}
 	return string(out)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Merovius/aoc_2022
 
 go 1.19
 
-require golang.org/x/exp v0.0.0-20221126150942-6ab00d035af9 // indirect
+require (
+	golang.org/x/exp v0.0.0-20221126150942-6ab00d035af9 // indirect
+	gonih.org v0.0.0-20220709142908-eee07dfc6929 // indirect
+)
