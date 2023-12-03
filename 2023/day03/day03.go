2f86c66245b95fdac72606a5e987df62646dcde9
Axel Wagner <mero@merovius.de>
2023-12-03 09:48:52 +0100

    2023/day03: add Go solution

---
+++ 2023/day03/day03.go
@@ -0,0 +1,101 @@
+//go:build goexperiment.rangefunc
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"unicode"
+
+	"github.com/Merovius/AdventOfCode/internal/grid"
+	"github.com/Merovius/AdventOfCode/internal/iter"
+	"github.com/Merovius/AdventOfCode/internal/set"
+)
+
+func main() {
+	g, err := grid.Read[rune](os.Stdin, func(r rune) (rune, error) { return r, nil })
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Sum of part numbers:", Part1(g))
+	fmt.Println("Sum of gear rations:", Part2(g))
+}
+
+func Part1(g *grid.Grid[rune]) int {
+	var sum int
+	for n := range FindPartNumbers(g) {
+		sum += n.N
+	}
+	return sum
+}
+
+func Part2(g *grid.Grid[rune]) int {
+	partNumbers := set.Collect(FindPartNumbers(g))
+
+	var sum int
+	for p := range grid.Find(g, '*') {
+		var adj []Number
+		for n := range partNumbers {
+			if n.Neighborhood().Contains(p) {
+				adj = append(adj, n)
+			}
+		}
+		if len(adj) != 2 {
+			continue
+		}
+		sum += adj[0].N * adj[1].N
+	}
+	return sum
+}
+
+type Number struct {
+	N int
+	R grid.Rectangle
+}
+
+func (n Number) Neighborhood() grid.Rectangle {
+	return n.R.Inset(-1)
+}
+
+func FindNumbers(g *grid.Grid[rune]) iter.Seq[Number] {
+	return func(yield func(Number) bool) {
+		p := grid.Pos{}
+		var b []rune
+		for p.Row = range g.H {
+			var n Number
+			for p.Col = 0; p.Col < g.W; p.Col++ {
+				n.R.Min, n.R.Max = p, p
+				for g.Valid(n.R.Max) && unicode.IsDigit(g.At(n.R.Max)) {
+					b = append(b, g.At(n.R.Max))
+					n.R.Max.Col++
+				}
+				if len(b) > 0 {
+					v, err := strconv.Atoi(string(b))
+					if err != nil {
+						panic(err)
+					}
+					p = n.R.Max
+					n.N = v
+					n.R.Max.Row++
+					if !yield(n) {
+						return
+					}
+					b = b[:0]
+				}
+			}
+		}
+	}
+}
+
+func FindPartNumbers(g *grid.Grid[rune]) iter.Seq[Number] {
+	return iter.Filter(FindNumbers(g), func(n Number) bool {
+		for _, r := range g.Rect(n.Neighborhood()) {
+			if r != '.' && !unicode.IsDigit(r) {
+				return true
+			}
+		}
+		return false
+	})
+}
---
+++ 2023/day03/day03_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	_ "embed"
+	"strings"
+	"testing"
+
+	"github.com/Merovius/AdventOfCode/internal/grid"
+)
+
+//go:embed example.txt
+var example string
+
+//go:embed input.txt
+var input string
+
+func Test(t *testing.T) {
+	tcs := []struct {
+		name      string
+		in        string
+		wantPart1 int
+		wantPart2 int
+	}{
+		{"example", example, 4361, 467835},
+		{"input", input, 531932, 73646890},
+	}
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			g, err := grid.Read[rune](strings.NewReader(tc.in), func(r rune) (rune, error) { return r, nil })
+			if err != nil {
+				t.Fatal(err)
+			}
+			if got := Part1(g); got != tc.wantPart1 {
+				t.Errorf("Part1() = %v, want %v", got, tc.wantPart1)
+			}
+			if got := Part2(g); got != tc.wantPart2 {
+				t.Errorf("Part2() = %v, want %v", got, tc.wantPart2)
+			}
+		})
+	}
+}
