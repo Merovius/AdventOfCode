d8ff058de69c817eca2c33cc3b2deaa583a2f7ff
Axel Wagner <mero@merovius.de>
2023-12-07 14:24:05 +0100

    2023/day07: simplify Go solution

--- 2023/day07/day07.go
+++ 2023/day07/day07.go
@@ -2,16 +2,15 @@ package main
 
 import (
 	"bytes"
-	"cmp"
 	"fmt"
 	"io"
 	"log"
 	"os"
-	"slices"
 	"strings"
 
 	"github.com/Merovius/AdventOfCode/internal/input/parse"
 	"github.com/Merovius/AdventOfCode/internal/input/split"
+	"github.com/Merovius/AdventOfCode/internal/slices"
 )
 
 func main() {
@@ -28,17 +27,13 @@ func main() {
 }
 
 func Parse(b []byte) ([]Player, error) {
-	players, err := parse.Lines(
+	return parse.Lines(
 		parse.Struct[Player](
 			split.Fields,
 			parse.Array[Hand, byte](split.Bytes, parse.Byte),
 			parse.Signed[int],
 		),
 	)(string(bytes.TrimSpace(b)))
-	if err != nil {
-		return nil, err
-	}
-	return players, nil
 }
 
 type Player struct {
@@ -49,38 +44,24 @@ type Player struct {
 type Hand [5]byte
 
 func Part1(players []Player) int {
-	players = slices.Clone(players)
-	slices.SortFunc(players, func(a, b Player) int {
-		return a.Hand.Cmp(b.Hand)
-	})
-	var total int
-	for r, p := range players {
-		total += (r + 1) * p.Bid
-	}
-	return total
+	return winnings(players, false)
 }
 
 func Part2(players []Player) int {
-	players = slices.Clone(players)
-	slices.SortFunc(players, func(a, b Player) int {
-		return a.Hand.Cmp2(b.Hand)
-	})
+	return winnings(players, true)
+}
+
+func winnings(players []Player, joker bool) int {
+	vals := make([]uint32, len(players))
+	for i, p := range players {
+		vals[i] = p.Hand.Value(joker)
+	}
+	slices.SortBy(players, vals)
 	var total int
 	for r, p := range players {
 		total += (r + 1) * p.Bid
 	}
 	return total
-	return 0
-}
-
-func CmpCards(a, b byte) int {
-	const strength = "23456789TJQKA"
-	return cmp.Compare(strings.IndexByte(strength, a), strings.IndexByte(strength, b))
-}
-
-func CmpCards2(a, b byte) int {
-	const strength = "J23456789TQKA"
-	return cmp.Compare(strings.IndexByte(strength, a), strings.IndexByte(strength, b))
 }
 
 type Type int
@@ -96,110 +77,62 @@ const (
 )
 
 func (h Hand) String() string {
-	slices.SortFunc(h[:], CmpCards)
 	return string(h[:])
 }
 
-func (h Hand) Type() Type {
-	// look at sorted hand. Every type we check for rules out the lower types.
-	slices.Sort(h[:])
-	if h[0] == h[4] {
-		return FiveOfAKind
-	}
-	if h[0] == h[3] || h[1] == h[4] {
-		return FourOfAKind
-	}
-	if (h[0] == h[2] && h[3] == h[4]) || (h[0] == h[1] && h[2] == h[4]) {
-		return FullHouse
-	}
-	if h[0] == h[2] || h[1] == h[3] || h[2] == h[4] {
-		return ThreeOfAKind
-	}
-	// count pairs
-	var np int
-	for i := 1; i < 5; i++ {
-		if h[i] == h[i-1] {
-			np++
-		}
-	}
-	switch np {
-	case 2:
-		return TwoPair
-	case 1:
-		return OnePair
-	default:
-		return HighCard
-	}
+const (
+	Order1 = "23456789TJQKA"
+	Order2 = "J23456789TQKA"
+)
+
+// handType maps the highest two counts of card values to the type of hand.
+var handType = map[[2]int]Type{
+	{5, 0}: FiveOfAKind,
+	{4, 1}: FourOfAKind,
+	{3, 2}: FullHouse,
+	{3, 1}: ThreeOfAKind,
+	{2, 2}: TwoPair,
+	{2, 1}: OnePair,
+	{1, 1}: HighCard,
 }
 
-func (h Hand) Type2() Type {
-	slices.Sort(h[:])
-
-	// remove jokers
-	r := strings.ReplaceAll(string(h[:]), "J", "")
-
-	if len(r) < 2 {
-		// 4 or 5 jokers
-		return FiveOfAKind
-	}
-	if len(r) == 2 {
-		// 3 jokers
-		if r[0] == r[1] {
-			return FiveOfAKind
+// Value returns a value for h that can be used to rank Hands. A better Hand
+// will always have a higher Value.
+func (h Hand) Value(joker bool) uint32 {
+	// Find the two most common card values. They determine the hand type.
+	var (
+		njokers int
+		maxC    [2]int
+	)
+	for i := 0; i < len(Order2); i++ {
+		c := bytes.Count(h[:], []byte{Order2[i]})
+		if joker && i == 0 {
+			njokers = c
+			continue
 		}
-		return FourOfAKind
-	}
-	if len(r) == 3 {
-		// 2 jokers
-		if r[0] == r[2] {
-			return FiveOfAKind
-		}
-		if r[0] == r[1] || r[1] == r[2] {
-			return FourOfAKind
-		}
-		// Full House can be re-jokered into Four of a kind
-		return ThreeOfAKind
-	}
-	if len(r) == 4 {
-		// 1 joker
-		if r[0] == r[3] {
-			return FiveOfAKind
-		}
-		if r[0] == r[2] || r[1] == r[3] {
-			return FourOfAKind
-		}
-		// count pairs
-		var np int
-		for i := 1; i < len(r); i++ {
-			if r[i] == r[i-1] {
-				np++
+		if c > maxC[1] {
+			if c > maxC[0] {
+				maxC[0], maxC[1] = c, maxC[0]
+			} else {
+				maxC[1] = c
 			}
 		}
-		switch np {
-		case 2:
-			return FullHouse
-		case 1:
-			return ThreeOfAKind
-		default:
-			return OnePair
-		}
 	}
-	// no jokers
-	return h.Type()
-}
+	ord := Order1
+	if joker {
+		// jokers should always boost the most common card value, as we don't
+		// have straights
+		maxC[0] += njokers
+		ord = Order2
+	}
 
-func (h Hand) Cmp(g Hand) int {
-	ht, gt := h.Type(), g.Type()
-	if c := cmp.Compare(ht, gt); c != 0 {
-		return c
+	// Encode the card values, in lexicographic order, as a Base 13 integer.
+	// The hand type is the most significant digit, so it dominates the
+	// magnitude of the value.
+	v := uint32(handType[maxC])
+	for _, b := range h {
+		v *= uint32(len(ord))
+		v += uint32(strings.IndexByte(ord, b))
 	}
-	return slices.CompareFunc(h[:], g[:], CmpCards)
-}
-
-func (h Hand) Cmp2(g Hand) int {
-	ht, gt := h.Type2(), g.Type2()
-	if c := cmp.Compare(ht, gt); c != 0 {
-		return c
-	}
-	return slices.CompareFunc(h[:], g[:], CmpCards2)
+	return v
 }
