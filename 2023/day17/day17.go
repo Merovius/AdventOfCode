9d3a11267203c04233b10d4657b4e924a8bdd709
Axel Wagner <mero@merovius.de>
2024-01-03 08:48:20 +0100

    2023/day17: do not track number of steps in node
    Instead of keeping track of the number of steps taken in the node, only
    keep track of the last direction and generate all allowed strides as
    edges. This reduces the number of nodes in the graph by a factor of 3
    and 7 respectively.
    
    Also, unify both parts into one function.
    
    Benchstat:
    
    goos: linux
    goarch: amd64
    pkg: github.com/Merovius/AdventOfCode/2023/day17
    cpu: Intel(R) Core(TM) i7-8700K CPU @ 3.70GHz
                          │   old.txt    │               new.txt               │
                          │    sec/op    │   sec/op     vs base                │
    Part1/WithParse-12       526.5m ± 1%   341.5m ± 1%  -35.15% (p=0.000 n=10)
    Part1/WithoutParse-12    527.3m ± 0%   340.6m ± 4%  -35.41% (p=0.000 n=10)
    Part2/WithParse-12      1501.0m ± 3%   882.3m ± 1%  -41.22% (p=0.000 n=10)
    Part2/WithoutParse-12   1501.2m ± 1%   886.1m ± 0%  -40.97% (p=0.000 n=10)
    geomean                  889.3m        549.1m       -38.25%

--- 2023/day17/day17.go
+++ 2023/day17/day17.go
@@ -38,10 +38,11 @@ func Parse(s string) (*Graph, error) {
 }
 
 func Part1(g *Graph) int {
+	g.Min = 0
+	g.Max = 3
 	start := Node{
 		P: grid.Pos{0, 0},
 		Δ: grid.Pos{},
-		N: 0,
 	}
 	path := graph.Dijkstra[Node, Edge, int](g, start, func(n Node) bool {
 		return n.P.Row == g.H-1 && n.P.Col == g.W-1
@@ -54,11 +55,11 @@ func Part1(g *Graph) int {
 }
 
 func Part2(g *Graph) int {
-	g.Ultra = true
+	g.Min = 3
+	g.Max = 10
 	start := Node{
 		P: grid.Pos{0, 0},
 		Δ: grid.Pos{},
-		N: 0,
 	}
 	path := graph.Dijkstra[Node, Edge, int](g, start, func(n Node) bool {
 		return n.P.Row == g.H-1 && n.P.Col == g.W-1
@@ -72,13 +73,13 @@ func Part2(g *Graph) int {
 
 type Graph struct {
 	*grid.Grid[int8]
-	Ultra bool
+	Min int
+	Max int
 }
 
 type Node struct {
 	P grid.Pos
 	Δ grid.Pos
-	N int
 }
 
 type Edge struct {
@@ -88,66 +89,29 @@ type Edge struct {
 }
 
 func (g *Graph) Edges(n Node) []Edge {
-	if g.Ultra {
-		return g.UltraEdges(n)
-	}
 	var out []Edge
 	for _, δ := range []grid.Pos{{1, 0}, {0, 1}, {-1, 0}, {0, -1}} {
-		q := n.P.Add(δ)
-		if !g.Valid(q) {
+		if δ == n.Δ || (δ.Row == -n.Δ.Row && δ.Col == -n.Δ.Col) {
 			continue
 		}
-		e := Edge{
-			From: n,
-			To:   Node{P: q, Δ: δ},
-			Loss: int(g.At(q)),
-		}
-		if δ.Row == -n.Δ.Row && δ.Col == -n.Δ.Col {
-			continue
-		} else if δ == n.Δ {
-			if n.N == 3 {
-				continue
+		var (
+			loss int
+			p    = n.P
+		)
+		for i := range g.Max {
+			p = p.Add(δ)
+			if !g.Valid(p) {
+				break
 			}
-			e.To.N = n.N + 1
-		} else {
-			e.To.N = 1
-		}
-		out = append(out, e)
-	}
-	return out
-}
-
-func (g *Graph) UltraEdges(n Node) []Edge {
-	var out []Edge
-	if n.N > 0 && n.N < 10 {
-		q := n.P.Add(n.Δ)
-		if g.Valid(q) {
-			out = append(out, Edge{
-				From: n,
-				To:   Node{q, n.Δ, n.N + 1},
-				Loss: int(g.At(q)),
-			})
-		}
-	}
-directions:
-	for _, δ := range []grid.Pos{{1, 0}, {0, 1}, {-1, 0}, {0, -1}} {
-		if δ == n.Δ || δ.Row == -n.Δ.Row && δ.Col == -n.Δ.Col {
-			continue
-		}
-		q := n.P
-		var loss int
-		for i := 0; i < 4; i++ {
-			q = q.Add(δ)
-			if !g.Valid(q) {
-				continue directions
+			loss += int(g.At(p))
+			if i >= g.Min {
+				out = append(out, Edge{
+					From: n,
+					To:   Node{p, δ},
+					Loss: loss,
+				})
 			}
-			loss += int(g.At(q))
 		}
-		out = append(out, Edge{
-			From: n,
-			To:   Node{q, δ, 4},
-			Loss: loss,
-		})
 	}
 	return out
 }
