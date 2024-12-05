bd8991f333c5a9bf915d51b90c188f870635a2e5
Axel Wagner <mero@merovius.de>
2024-12-05 10:17:29 +0100

    2024/day05: acknowledge limitations

--- 2024/day05/day05.go
+++ 2024/day05/day05.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math/rand/v2"
 	"os"
 	"slices"
 
@@ -46,6 +47,18 @@ func main() {
 		g.Set(grid.Pos{r[0], r[1]}, -1)
 		g.Set(grid.Pos{r[1], r[0]}, 1)
 	}
+	// Note: Technically, this is incorrect. The input could only specify a
+	// partial order and it would have to be made into a transitive closure.
+	// That is, the input could be
+	//     A|B
+	//     B|C
+	//
+	//     B,X,A
+	//     C,A,B
+	// Which would be two unsorted series, but the relation we are building
+	// would not "see" either of them.
+	// However, it turns out our input specifies a total ordering for all
+	// the actually appearing sequences, so…
 	cmp := func(a, b int) int {
 		return g.At(grid.Pos{a, b})
 	}
@@ -57,6 +70,10 @@ func main() {
 		if slices.IsSortedFunc(p, cmp) {
 			n += p[len(p)/2]
 		} else {
+			rand.Shuffle(len(p), func(i, j int) {
+				p[i], p[j] = p[j], p[i]
+			})
+
 			slices.SortFunc(p, cmp)
 			m += p[len(p)/2]
 		}
