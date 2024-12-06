b68eab28395a2740d77179c1afcc1d596b5bff19
Axel Wagner <mero@merovius.de>
2024-12-06 07:29:42 +0100

    internal/set: remove rangefunc build tag

--- internal/set/set.go
+++ internal/set/set.go
@@ -1,5 +1,7 @@
 package set
 
+import "iter"
+
 type Set[E comparable] map[E]struct{}
 
 func Make[E comparable](e ...E) Set[E] {
@@ -31,6 +33,16 @@ func (s Set[E]) Slice() []E {
 	return out
 }
 
+func (s Set[E]) All() iter.Seq[E] {
+	return func(yield func(E) bool) {
+		for e := range s {
+			if !yield(e) {
+				return
+			}
+		}
+	}
+}
+
 func Intersect[E comparable](s ...Set[E]) Set[E] {
 	if len(s) == 0 {
 		panic("empty intersection")
@@ -43,6 +55,14 @@ loop:
 				continue loop
 			}
 		}
+		out.Add(e)
+	}
+	return out
+}
+
+func Collect[E comparable](s iter.Seq[E]) Set[E] {
+	out := make(Set[E])
+	for e := range s {
 		out.Add(e)
 	}
 	return out
