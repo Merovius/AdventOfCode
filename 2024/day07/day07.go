b9ed1b0899c2102b590bc276d033b09487613aa3
Axel Wagner <mero@merovius.de>
2024-12-11 11:55:52 +0100

    2024/day07: make testable

--- 2024/day07/day07.go
+++ 2024/day07/day07.go
@@ -15,31 +15,38 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	in, err := parse.Lines(parse.Struct[Equation](split.On(": "), parse.Signed[int], parse.Slice(split.Fields, parse.Signed[int])))(string(buf))
+	in, err := Parse(buf)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	fmt.Println(Part1(in))
+	fmt.Println(Part2(in))
+}
+
+type Equation struct {
+	Test   int
+	Inputs []int
+}
+
+func Parse(in []byte) ([]Equation, error) {
+	return parse.Lines(
+		parse.Struct[Equation](
+			split.On(": "),
+			parse.Signed[int],
+			parse.Slice(split.Fields, parse.Signed[int]),
+		),
+	)(string(in))
+}
+
+func Part1(in []Equation) int {
 	var total int
 	for _, e := range in {
 		if solvable(e.Test, e.Inputs) {
 			total += e.Test
 		}
 	}
-	fmt.Println(total)
-
-	total = 0
-	for _, e := range in {
-		if solvable2(e.Test, e.Inputs) {
-			total += e.Test
-		}
-	}
-	fmt.Println(total)
-}
-
-type Equation struct {
-	Test   int
-	Inputs []int
+	return total
 }
 
 func solvable(res int, ins []int) bool {
@@ -51,6 +58,16 @@ func solvable(res int, ins []int) bool {
 		return true
 	}
 	return res%n == 0 && solvable(res/n, ins)
+}
+
+func Part2(in []Equation) int {
+	var total int
+	for _, e := range in {
+		if solvable2(e.Test, e.Inputs) {
+			total += e.Test
+		}
+	}
+	return total
 }
 
 func solvable2(res int, ins []int) bool {
