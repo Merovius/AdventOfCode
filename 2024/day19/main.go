d0d592f2b5d1aa7fb4e2ef39bd7998a77a680d34
Axel Wagner <mero@merovius.de>
2024-12-19 15:18:49 +0100

    2024/day19: memoize over pattern-length
    goos: linux
    goarch: amd64
    pkg: github.com/Merovius/AdventOfCode/2024/day19
    cpu: Intel(R) Core(TM) i7-8700K CPU @ 3.70GHz
             │ /tmp/base.txt  │             /tmp/radix.txt             │             /tmp/memo.txt             │
             │     sec/op     │    sec/op      vs base                 │    sec/op     vs base                 │
    Parse-12     88.89µ ± 37%   402.92µ ±  1%  +353.26% (p=0.000 n=10)   401.50µ ± 1%  +351.67% (p=0.000 n=10)
    Part1-12   14908.8µ ±  0%   4924.2µ ± 11%   -66.97% (p=0.000 n=10)    674.2µ ± 1%   -95.48% (p=0.000 n=10)
    Part2-12    40.173m ± 23%    5.302m ± 11%   -86.80% (p=0.000 n=10)    1.115m ± 1%   -97.23% (p=0.000 n=10)
    geomean      3.762m          2.191m         -41.76%                   670.7µ        -82.17%

--- 2024/day19/main.go
+++ 2024/day19/main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"slices"
 	"strings"
 
 	"github.com/Merovius/AdventOfCode/internal/container"
@@ -50,11 +51,12 @@ type Input struct {
 }
 
 func Part1(in Input) int {
-	memo := make(map[string]int)
+	m := new(memo)
 
 	var n int
 	for _, d := range in.Designs {
-		if count(memo, true, in.Patterns, d) > 0 {
+		m.Clear()
+		if count(m, true, in.Patterns, d) > 0 {
 			n++
 		}
 	}
@@ -62,32 +64,57 @@ func Part1(in Input) int {
 }
 
 func Part2(in Input) int {
-	memo := make(map[string]int)
+	m := new(memo)
 
 	var n int
 	for _, d := range in.Designs {
-		n += count(memo, false, in.Patterns, d)
+		m.Clear()
+		n += count(m, false, in.Patterns, d)
 	}
 	return n
 }
 
-var memo map[string]int
-
-func count(memo map[string]int, checkOnly bool, patterns *container.RadixSet, design string) (v int) {
-	if v, ok := memo[design]; ok {
+func count(m *memo, checkOnly bool, patterns *container.RadixSet, design string) (v int) {
+	if v, ok := m.Get(design); ok {
 		return v
 	}
-	defer func() { memo[design] = v }()
+	defer func() { m.Set(design, v) }()
 
 	if len(design) == 0 {
 		return 1
 	}
 	var n int
 	for p := range patterns.PrefixesOf(design) {
-		n += count(memo, checkOnly, patterns, design[len(p):])
+		n += count(m, checkOnly, patterns, design[len(p):])
 		if checkOnly && n > 0 {
 			return 1
 		}
 	}
 	return n
+}
+
+// memo memoizes the result of count. If we memoize per-pattern, we can just
+// use the length of the considered suffix as a key. This avoids having to
+// hash and allocate. It duplicates work, but is still a big overall win.
+//
+// To simplify clearing, we store the return value offset by one, so 0
+// signifies "no value".
+type memo []int
+
+func (m *memo) Get(s string) (int, bool) {
+	if len(*m) > len(s) && (*m)[len(s)] > 0 {
+		return (*m)[len(s)] - 1, true
+	}
+	return 0, false
+}
+
+func (m *memo) Set(s string, v int) {
+	if len(s) >= len(*m) {
+		*m = slices.Grow(*m, len(s)+1)[:len(s)+1]
+	}
+	(*m)[len(s)] = v + 1
+}
+
+func (m *memo) Clear() {
+	clear(*m)
 }
