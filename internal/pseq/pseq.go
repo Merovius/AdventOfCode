6636400a25cae7240cff913c1f2fca0d69fe17dc
Axel Wagner <mero@merovius.de>
2024-12-22 12:31:21 +0100

    internal/pseq: add Each/Each2

--- internal/pseq/pseq.go
+++ internal/pseq/pseq.go
@@ -7,6 +7,11 @@ import (
 	"sync"
 )
 
+type pair[A, B any] struct {
+	a A
+	b B
+}
+
 // MapMerge runs f in parallel, merging the results using m. m must be
 // associative and commutative.
 func MapMerge[A, B any](seq iter.Seq[A], f func(A) B, m func(B, B) B) B {
@@ -42,4 +47,48 @@ func MapMerge[A, B any](seq iter.Seq[A], f func(A) B, m func(B, B) B) B {
 		b = m(b, *bs[i])
 	}
 	return b
+}
+
+// Each runs f in parallel, passing in all elements from seq.
+func Each[A any](seq iter.Seq[A], f func(A)) {
+	N := runtime.NumCPU()
+	chA := make(chan A, N)
+	wg := new(sync.WaitGroup)
+	wg.Add(N)
+	for range N {
+		go func(wg *sync.WaitGroup, in <-chan A, f func(A)) {
+			defer wg.Done()
+			for a := range in {
+				f(a)
+			}
+		}(wg, chA, f)
+	}
+	for a := range seq {
+		chA <- a
+	}
+	close(chA)
+	wg.Wait()
+}
+
+// Each2 runs f in parallel, passing in all pairs from seq.
+func Each2[A, B any](seq iter.Seq2[A, B], f func(A, B)) {
+	// not simply using Each[pair[A,B]], as it requires allocating a
+	// closure.
+	N := runtime.NumCPU()
+	chP := make(chan pair[A, B], N)
+	wg := new(sync.WaitGroup)
+	wg.Add(N)
+	for range N {
+		go func(wg *sync.WaitGroup, in <-chan pair[A, B], f func(A, B)) {
+			defer wg.Done()
+			for p := range in {
+				f(p.a, p.b)
+			}
+		}(wg, chP, f)
+	}
+	for a, b := range seq {
+		chP <- pair[A, B]{a, b}
+	}
+	close(chP)
+	wg.Wait()
 }
