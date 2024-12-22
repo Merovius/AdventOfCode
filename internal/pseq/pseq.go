e1c79ef2af920dc970d9bd1e909780fac3849d60
Axel Wagner <mero@merovius.de>
2024-12-22 17:20:36 +0100

    internal/pseq: add MapReduce

--- internal/pseq/pseq.go
+++ internal/pseq/pseq.go
@@ -92,3 +92,95 @@ func Each2[A, B any](seq iter.Seq2[A, B], f func(A, B)) {
 	close(chP)
 	wg.Wait()
 }
+
+// MapReduce runs a MapReduce pipeline. It runs m (the map phase) in parallel
+// to split the sequence of inputs into key/value pairs. It then spawns one
+// goroutine running r per key (the reduce phase). Lastly, the outputs of all
+// reducers are output as a sequence.
+func MapReduce[A, B, C any, K comparable](seq iter.Seq[A], m func(A) iter.Seq2[K, B], r func(K, iter.Seq[B]) C) iter.Seq2[K, C] {
+	return func(yield func(K, C) bool) {
+		N := runtime.NumCPU()
+
+		chA := make(chan A, N)
+		chKB := make(chan pair[K, B], N)
+		chKC := make(chan pair[K, C])
+		stop := make(chan struct{})
+
+		// map
+		wg := new(sync.WaitGroup)
+		wg.Add(N)
+		for range N {
+			go func(wg *sync.WaitGroup, in <-chan A, m func(A) iter.Seq2[K, B], out chan<- pair[K, B]) {
+				defer wg.Done()
+				for a := range in {
+					for k, b := range m(a) {
+						out <- pair[K, B]{k, b}
+					}
+				}
+			}(wg, chA, m, chKB)
+		}
+		go func(wg *sync.WaitGroup, thenClose chan<- pair[K, B]) {
+			wg.Wait()
+			close(thenClose)
+		}(wg, chKB)
+
+		// shuffle
+		wg = new(sync.WaitGroup)
+		wg.Add(1)
+		go func(wg *sync.WaitGroup, in <-chan pair[K, B], r func(K, iter.Seq[B]) C, chKC chan<- pair[K, C]) {
+			defer wg.Done()
+			reducers := make(map[K]chan<- B)
+			for kb := range in {
+				k, b := kb.a, kb.b
+				if chB, ok := reducers[k]; ok {
+					chB <- b
+					continue
+				}
+
+				// reduce
+				chB := make(chan B)
+				defer close(chB)
+				wg.Add(1)
+				go func(wg *sync.WaitGroup, k K, in <-chan B, r func(K, iter.Seq[B]) C, out chan<- pair[K, C]) {
+					defer wg.Done()
+					c := r(k, func(yield func(B) bool) {
+						var stopped bool
+						for b := range in {
+							if !stopped && !yield(b) {
+								// TODO: figure out if we want to do something else here.
+								stopped = true
+							}
+						}
+					})
+					out <- pair[K, C]{k, c}
+				}(wg, k, chB, r, chKC)
+				reducers[k] = chB
+				chB <- b
+			}
+		}(wg, chKB, r, chKC)
+		go func(wg *sync.WaitGroup, thenClose chan<- pair[K, C]) {
+			wg.Wait()
+			close(thenClose)
+		}(wg, chKC)
+
+		// consume input sequence
+		go func(stop <-chan struct{}, seq iter.Seq[A], out chan<- A) {
+			defer close(out)
+			for a := range seq {
+				select {
+				case out <- a:
+				case <-stop:
+					return
+				}
+			}
+		}(stop, seq, chA)
+
+		var stopped bool
+		for kc := range chKC {
+			if !stopped && !yield(kc.a, kc.b) {
+				close(stop)
+				stopped = true
+			}
+		}
+	}
+}
